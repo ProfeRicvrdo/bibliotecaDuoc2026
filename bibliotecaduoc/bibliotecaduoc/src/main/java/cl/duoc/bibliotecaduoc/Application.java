3cff37f94337e570fdf543fd6c28a999d0db07c3
ProfeRicvrdo <ri.yaneza@profesor.duoc.cl>
2026-03-19 09:59:27 -0300

    Update Application.java

--- bibliotecaduoc/bibliotecaduoc/src/main/java/cl/duoc/bibliotecaduoc/Application.java
+++ bibliotecaduoc/bibliotecaduoc/src/main/java/cl/duoc/bibliotecaduoc/Application.java
@@ -9,9 +9,8 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
-
-
-
+		System.out.println("aaaaaaaaaaaaaaaaaa");
+		System.out.println("Esto es un cambio de prueba para git");
 
 
 
